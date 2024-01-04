f7b5c21eaea80dfb680c1011580cba0d100938fd
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-01-04 11:02:26 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,13 +43,13 @@ feedbackOptions = {
         style={{
           height: '100vh',
           display: 'flex',
+          flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: 40,
           color: '#010101',
         }}
       >
-        {/* <Feedback /> */}
         <FeedbackOptions
           options={this.feedbackOptions}
           onLeaveFeedback={this.handleButtonClick}
--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -1,10 +1,23 @@
-export const FeedbackOptions = props => {
+// export const FeedbackOptions = props => {
+//   return (
+//     <div>
+//       <h2>Please leave feedback</h2>
+//       {props.feedbackOptions.map(option => {
+//         <button onClick={() => props.handleButtonClick(option)}>{option}</button>;
+//       })}
+//     </div>
+//   );
+// };
+
+export const FeedbackOptions = (props) => {
+  const options = Object.keys(props.options);
+
   return (
     <div>
       <h2>Please leave feedback</h2>
-      {props.feedbackOptions.map(option => {
-        <button onClick={() => props.handleButtonClick(option)}>{option}</button>;
-      })}
+      {options.map((option) => (
+        <button key={option} onClick={() => props.handleButtonClick(option)}>{option}</button>
+      ))}
     </div>
   );
 };
